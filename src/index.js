7305c24087b43391469dc544c81562601c6ae38d
CharlesAD <c.acquahdavis@gamil.com>
2022-08-15 17:25:41 +0100

    Prevent the default browser submission event

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-console.loe("hello")
+// Log the form submission
+
+// Select the form element from the HTML
+const form = document.querySelector("form"); // "document" basically is the HTML document. The querySelector basically goes through the HTML document and returns the first selector that matches the thing searched.
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  console.log("Form submitted");
+});
